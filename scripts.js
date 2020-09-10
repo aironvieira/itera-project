57400258fbf3a24fe58de8ad93a22b24ab8baa6a
Matheus Airon <ma.aironvieira@gmail.com>
2020-09-10 13:46:29 -0300

    commons and header

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+var nav_menu = document.querySelector('#nav-icon3');  
+var header = document.querySelector('#header');
+
+nav_menu.addEventListener('click', function(){
+    nav_menu.classList.toggle('open');
+    header.classList.toggle('open');
+});
+
+window.addEventListener('scroll', function(){
+    if(window.pageYOffset > 50){
+        header.classList.add('back-fixed');
+    }else{
+        header.classList.remove('back-fixed');
+    }  
+});
+
+
+
+
