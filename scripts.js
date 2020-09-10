4551f3c8c9993e2970f22ad0d92c698c2b58bbcf
Matheus Airon <ma.aironvieira@gmail.com>
2020-09-10 15:40:16 -0300

    footer and hotfixes

--- scripts.js
+++ scripts.js
@@ -1,20 +1,23 @@
 
-var nav_menu = document.querySelector('#nav-icon3');  
-var header = document.querySelector('#header');
+document.addEventListener('DOMContentLoaded', (event) => {
 
-nav_menu.addEventListener('click', function(){
-    nav_menu.classList.toggle('open');
-    header.classList.toggle('open');
+    var nav_menu = document.querySelector('#nav-icon3');  
+    var header = document.querySelector('#header');
+
+    nav_menu.addEventListener('click', function(){
+        nav_menu.classList.toggle('open');
+        header.classList.toggle('open');
+    });
+
+    window.addEventListener('scroll', function(){
+        if(window.pageYOffset > 50){
+            header.classList.add('back-fixed');
+        }else{
+            header.classList.remove('back-fixed');
+        }  
+    });
+
 });
-
-window.addEventListener('scroll', function(){
-    if(window.pageYOffset > 50){
-        header.classList.add('back-fixed');
-    }else{
-        header.classList.remove('back-fixed');
-    }  
-});
-
 
 
 
